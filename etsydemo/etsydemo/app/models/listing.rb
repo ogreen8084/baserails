4f1fb0533f6556b822f8b2ce4760fe2bf76dc357
Oscar Green <fsug@yahoo.com>
2015-11-19 19:55:49 -0500

    add figaro gem

--- etsydemo/etsydemo/app/models/listing.rb
+++ etsydemo/etsydemo/app/models/listing.rb
@@ -1,4 +1,8 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :images, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "NoImageAvailable.jpg"
+  has_attached_file :images, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "NoImageAvailable.jpg",
+
+    :storage => :dropbox,
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
   validates_attachment_content_type :images, content_type: /\Aimage\/.*\Z/
 end
