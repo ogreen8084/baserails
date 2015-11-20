4d6a6de5376c3aed1b5378167c6d8fa541144734
Oscar Green <fsug@yahoo.com>
2015-11-20 05:52:40 -0500

    Install and set up devise gem

--- etsydemo/etsydemo/app/models/listing.rb
+++ etsydemo/etsydemo/app/models/listing.rb
@@ -1,8 +1,13 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :images, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "NoImageAvailable.jpg",
+  if Rails.env.development?
+    has_attached_file :images, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "NoImageAvailable.jpg"
+  else
+    has_attached_file :images, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "NoImageAvailable.jpg",
 
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+      :storage => :dropbox,
+      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+      :path => ":style/:id_:filename"
+  end
 
   validates_attachment_content_type :images, content_type: /\Aimage\/.*\Z/
 end
