c5221d5ce050d84b195d4e80a1b2a4b3a8f7e60e
Oscar Green <fsug@yahoo.com>
2015-11-20 20:04:29 -0500

    set user permission

--- etsydemo/etsydemo/app/controllers/listings_controller.rb
+++ etsydemo/etsydemo/app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_filter :check_user, only: [:edit, :update, :destroy]
   # GET /listings
   # GET /listings.json
   def index
@@ -20,6 +21,7 @@ class ListingsController < ApplicationController
   # GET /listings/1/edit
   def edit
   end
+
 
   # POST /listings
   # POST /listings.json
@@ -71,5 +73,11 @@ class ListingsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def listing_params
       params.require(:listing).permit(:name, :description, :price, :images)
+    end
+
+    def check_user
+      if current_user != @listing.user
+        redirect_to root_url, alert: "Sorry, this listing belongs to someone else!"
+      end
     end
 end
